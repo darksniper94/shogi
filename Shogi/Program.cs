574cbe27ae945a9c7006ff928c61d4af63c5ba31
HonigweinPanda <vanessa.griesmer@gmx.de>
2015-03-28 16:45:24 +0100

    Registrierungs- und Anmeldedialogfenster //ohne funktionalitäten

--- Shogi/Program.cs
+++ Shogi/Program.cs
@@ -15,7 +15,7 @@ namespace Shogi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormAnmeldung());
 
             //Position p = new Position('A', '1');
 
---
+++ Shogi/Registrierung.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shogi
+{
+    public partial class FormRegistrierung : Form
+    {
+        public FormRegistrierung()
+        {
+            InitializeComponent();
+        }
+
+        private void formRegistrierung_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bAbbrechen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bRegistrieren_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
