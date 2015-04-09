adceb4afbaf9cbb3bd723a78418ff5f10ee26dc5
darksniper94 <alexander.agasiev@gmail.com>
2015-04-09 23:47:41 +0200

    Datenbank und DLL hinzugefügt

---
+++ Shogi/Database.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Shogi
+{
+    class Database
+    {
+        private SQLiteConnection connection;
+        private const String DBNAME = "data.dat";
+        private static Database inst = null;
+
+        private Database()
+        {
+            // Create of Open Database
+            bool createFile = !File.Exists(DBNAME);
+            if (createFile)
+            {
+                SQLiteConnection.CreateFile(DBNAME);
+            }
+            connection = new SQLiteConnection("Data Source="+DBNAME+";Version=3;");
+            connection.Open();
+
+            // Create Tables
+            if(createFile)
+            {
+                createTables();
+            }
+            
+        }
+
+        private void createTables()
+        {
+
+        }
+
+        public static Database instance
+        {
+            get
+            {
+                if(inst==null)
+                {
+                    inst = new Database();
+                }
+                return instance;
+            }
+        }
+    }
+}
