8af89a1d025ea15faf0a8d2e28e81eec71a29cde
ConnorVD <tobias.schuster92@gmail.com>
2015-04-20 10:58:12 +0200

    Figurtyp erweitert

---
+++ Shogi/Bewegungsmuster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shogi
+{
+    public class Bewegungsmuster
+    {
+        private int[] muster;
+
+        public Bewegungsmuster(int[] muster)
+        {
+            this.muster = muster;
+        }
+    }
+}
--- Shogi/FigurTyp.cs
+++ Shogi/FigurTyp.cs
@@ -6,6 +6,34 @@ using System.Text;
 namespace Shogi
 {
     public class FigurTyp
-    {
+    {                                                                            // Beginnt links oben, danach im Uhrzeigersinn
+        public static readonly FigurTyp KOENIG = new FigurTyp("König", new Bewegungsmuster(new[] {1, 1, 1, 1, 1, 1, 1, 1}));
+        public static readonly FigurTyp TURM = new FigurTyp("König", new Bewegungsmuster(new[] { 0, 9, 0, 9, 0, 9, 0, 9 }));
+        public static readonly FigurTyp LAEUFER = new FigurTyp("König", new Bewegungsmuster(new[] { 9, 0, 9, 0, 9, 0, 9, 0 }));
+        public static readonly FigurTyp GOLDGENERAL = new FigurTyp("König", new Bewegungsmuster(new[] { 1, 1, 1, 1, 0, 1, 0, 1 }));
+        public static readonly FigurTyp SILBGENERAL = new FigurTyp("König", new Bewegungsmuster(new[] { 1, 1, 1, 0, 1, 0, 1, 0 }));
+        //public static readonly FigurTyp SPRINGER = new FigurTyp("König", new Bewegungsmuster(new[] {}));
+        public static readonly FigurTyp LANZE = new FigurTyp("König", new Bewegungsmuster(new[] { 0, 9, 0, 0, 0, 0, 0, 0 }));
+        public static readonly FigurTyp BAUER = new FigurTyp("König", new Bewegungsmuster(new[] { 0, 1, 0, 0, 0, 0, 0, 0 }));
+        public static readonly FigurTyp DRACHE = new FigurTyp("König", new Bewegungsmuster(new[] { 1, 9, 1, 9, 1, 9, 1, 9 }));
+        public static readonly FigurTyp PFERD = new FigurTyp("König", new Bewegungsmuster(new[] { 9, 1, 9, 1, 9, 1, 9, 1 }));
+
+        private String name;
+        private Bewegungsmuster bewegungsmuster;
+
+        private FigurTyp(String name, Bewegungsmuster muster)
+        {
+            this.name = name;
+            this.bewegungsmuster = muster;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public Bewegungsmuster getBewegungsmuster() {
+            return this.bewegungsmuster;
+        }
     }
 }
