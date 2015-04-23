325ca709cc15a48a7cd15633703766878bbf7fb2
ConnorVD <tobias.schuster92@gmail.com>
2015-04-23 13:18:33 +0200

    FigurTyp überarbeitet

--- Shogi/Bewegungsmuster.cs
+++ Shogi/Bewegungsmuster.cs
@@ -7,11 +7,48 @@ namespace Shogi
 {
     public class Bewegungsmuster
     {
-        private int[] muster;
+        private List<Tuple<int, int>> muster;
 
-        public Bewegungsmuster(int[] muster)
+        public Bewegungsmuster(int[,] muster)
         {
-            this.muster = muster;
+            this.muster = erstelleListe(muster);
         }
+
+        public List<Tuple<int, int>> Muster
+        {
+            get { return this.muster; }
+        }
+
+        public Tuple<int, int> getItem(int index)
+        {
+            return muster.ElementAt(index);
+        }
+
+        private List<Tuple<int, int>> erstelleListe(int[,] muster)
+        {
+            List<Tuple<int, int>> liste = new List<Tuple<int, int>>();
+            Tuple<int, int> tuple;
+
+            for (int i = 0; i < muster.Length/2; ++i)
+            {
+                tuple = new Tuple<int, int>(muster[i, 0], muster[i, 1]);
+                liste.Add(tuple);
+            }
+
+            return liste;
+        }
+
+        public String ToString()
+        {
+            String ausgabe = "";
+
+            for (int i = 0; i < muster.Count; ++i)
+            {
+                ausgabe += muster.ElementAt(i) + "\n";
+            }
+
+            return ausgabe;
+        }
+
     }
 }
--- Shogi/FigurTyp.cs
+++ Shogi/FigurTyp.cs
@@ -6,17 +6,28 @@ using System.Text;
 namespace Shogi
 {
     public class FigurTyp
-    {                                                                            // Beginnt links oben, danach im Uhrzeigersinn
-        public static readonly FigurTyp KOENIG = new FigurTyp("König", new Bewegungsmuster(new[] {1, 1, 1, 1, 1, 1, 1, 1}));
-        public static readonly FigurTyp TURM = new FigurTyp("König", new Bewegungsmuster(new[] { 0, 9, 0, 9, 0, 9, 0, 9 }));
-        public static readonly FigurTyp LAEUFER = new FigurTyp("König", new Bewegungsmuster(new[] { 9, 0, 9, 0, 9, 0, 9, 0 }));
-        public static readonly FigurTyp GOLDGENERAL = new FigurTyp("König", new Bewegungsmuster(new[] { 1, 1, 1, 1, 0, 1, 0, 1 }));
-        public static readonly FigurTyp SILBGENERAL = new FigurTyp("König", new Bewegungsmuster(new[] { 1, 1, 1, 0, 1, 0, 1, 0 }));
-        //public static readonly FigurTyp SPRINGER = new FigurTyp("König", new Bewegungsmuster(new[] {}));
-        public static readonly FigurTyp LANZE = new FigurTyp("König", new Bewegungsmuster(new[] { 0, 9, 0, 0, 0, 0, 0, 0 }));
-        public static readonly FigurTyp BAUER = new FigurTyp("König", new Bewegungsmuster(new[] { 0, 1, 0, 0, 0, 0, 0, 0 }));
-        public static readonly FigurTyp DRACHE = new FigurTyp("König", new Bewegungsmuster(new[] { 1, 9, 1, 9, 1, 9, 1, 9 }));
-        public static readonly FigurTyp PFERD = new FigurTyp("König", new Bewegungsmuster(new[] { 9, 1, 9, 1, 9, 1, 9, 1 }));
+    {
+        private static readonly Bewegungsmuster MUSTERKOENIG = new Bewegungsmuster(new[,] { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 }, { -1, 0 } });
+        private static readonly Bewegungsmuster MUSTERTURM = new Bewegungsmuster(new[,] { { 0, -9 }, { 0, 9 }, { 9, 0 }, { -9, 0 } });
+        private static readonly Bewegungsmuster MUSTERLAEUFER = new Bewegungsmuster(new[,] { { -9, -9 }, { -9, 9 }, { 9, 9 }, { 9, -9 } });
+        private static readonly Bewegungsmuster MUSTERGOLDGENERAL = new Bewegungsmuster(new[,] { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } });
+        private static readonly Bewegungsmuster MUSTERSILBGENERAL = new Bewegungsmuster(new[,] { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 1 }, { -1, 1 } });
+        private static readonly Bewegungsmuster MUSTERSPRINGER = new Bewegungsmuster(new[,] { { -1, -2 }, { 1, -2 } });
+        private static readonly Bewegungsmuster MUSTERLANZE = new Bewegungsmuster(new[,] { { 0, -9 } });
+        private static readonly Bewegungsmuster MUSTERBAUER = new Bewegungsmuster(new[,] { { 0, -1 } });
+        private static readonly Bewegungsmuster MUSTERDRACHE = new Bewegungsmuster(new[,] { { -1, -1 }, { 0, -9 }, { 1, -1 }, { 9, 0 }, { 1, 1 }, { 0, 9 }, { -1, 1 }, { -9, 0 } });
+        private static readonly Bewegungsmuster MUSTERPFERD = new Bewegungsmuster(new[,] { { -9, -9 }, { 0, -1 }, { 9, -9 }, { 1, 0 }, { 9, 9 }, { 0, 1 }, { -9, 9 }, { -1, 0 } });
+        
+        public static readonly FigurTyp KOENIG = new FigurTyp("König", MUSTERKOENIG);
+        public static readonly FigurTyp TURM = new FigurTyp("Turm", MUSTERTURM);
+        public static readonly FigurTyp LAEUFER = new FigurTyp("Läufer", MUSTERLAEUFER);
+        public static readonly FigurTyp GOLDGENERAL = new FigurTyp("Goldener General", MUSTERGOLDGENERAL);
+        public static readonly FigurTyp SILBGENERAL = new FigurTyp("Silberner General", MUSTERSILBGENERAL);
+        public static readonly FigurTyp SPRINGER = new FigurTyp("Springer", MUSTERSPRINGER);
+        public static readonly FigurTyp LANZE = new FigurTyp("Lanze", MUSTERLANZE);
+        public static readonly FigurTyp BAUER = new FigurTyp("Bauer", MUSTERBAUER);
+        public static readonly FigurTyp DRACHE = new FigurTyp("Drache", MUSTERDRACHE);
+        public static readonly FigurTyp PFERD = new FigurTyp("Pferd", MUSTERPFERD);
 
         private String name;
         private Bewegungsmuster bewegungsmuster;
