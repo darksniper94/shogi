a0c50a80e37d71b8736dd361d6e1204978be88f8
HonigweinPanda <vanessa.griesmer@gmx.de>
2015-05-20 19:17:30 +0200

    farbe

--- Shogi/Farbewaehlen.cs
+++ Shogi/Farbewaehlen.cs
@@ -11,18 +11,20 @@ namespace Shogi
 {
     public partial class Farbewaehlen : Form
     {
+        TableLayoutPanel pnlTmp;
         Spieler spAngemeldet;
         public Farbewaehlen(Spieler paSpAngmeldet)
         {
             InitializeComponent();
             this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
             spAngemeldet = paSpAngmeldet;
-            TableLayoutPanel pnlTmp = new TableLayoutPanel();
+            pnlTmp = new TableLayoutPanel();
             pnlTmp.RowCount = 2;
             pnlTmp.ColumnCount = 2;
             pnlTmp.Padding = new Padding(1);
             pnlTmp.BackColor =  Color.FromArgb(170, 130, 70);
-            pnlTmp.Location = new Point(220, 50);
+            pnlTmp.Location = new Point(180, 50);
+            pnlTmp.Size = new Size(114, 114);
             Panel[] pnl = new Panel[4];
             for (int i = 0; i < 4; i++)
             {
@@ -31,7 +33,7 @@ namespace Shogi
                 pnl[i].Size = new Size(50, 50);
                 pnlTmp.Controls.Add(pnl[i]);
             }
-
+            rBtnStandard.Checked = true;
             this.Controls.Add(pnlTmp);
         }
 
@@ -58,6 +60,54 @@ namespace Shogi
                 spAngemeldet.farbe = "W";
             }
 
+            this.Close();
+        }
+
+        private void rBtnStandard_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in pnlTmp.Controls)
+            {
+                c.BackColor = Color.FromArgb(244, 223, 186);
+
+            }
+        }
+
+        private void rBtnHellblau_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in pnlTmp.Controls)
+            {
+                c.BackColor = Color.FromArgb(46, 143, 255);
+
+            }
+        }
+
+        private void rBtnHellgruen_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in pnlTmp.Controls)
+            {
+                c.BackColor = Color.FromArgb(39, 181, 0);
+            }
+        }
+
+        private void rBtnWeiss_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in pnlTmp.Controls)
+            {
+                c.BackColor = Color.FromArgb(255, 255, 255);
+            }
+        }
+
+        private void rBtnGrau_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in pnlTmp.Controls)
+            {
+                c.BackColor = Color.FromArgb(140, 140, 140);
+            }
+        }
+
+        private void btnAbbrechen_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
